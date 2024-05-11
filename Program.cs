431b56e4b15db44fd2273b4d347c3ae391aa206f
Center <948816115@qq.com>
2024-05-11 09:50:33 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -178,7 +178,7 @@ async Task Notify(string msg, bool isFailed = false)
     Console.WriteLine(msg);
     if (_conf.ScType == "Always" || (isFailed && _conf.ScType == "Failed"))
     {
-        await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?text={msg}");
+        await _scClient.GetAsync($"https://wxpusher.zjiecode.com/api/send/message/?appToken=AT_tOeRjm5Dsld5jzZ8zaw6apciH9DT1Q9b&content={msg}&uid={_conf.ScKey}&url=http%3a%2f%2fwxpusher.zjiecode.com");
     }
 }
 
